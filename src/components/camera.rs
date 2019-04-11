5ad2bc9bb7be66a536914e97ac0cbd9814041985
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-04-11 20:46:12 +0200

    Remove AmethystCamera field from Camera

--- src/components/camera.rs
+++ src/components/camera.rs
@@ -1,11 +1,9 @@
 use amethyst::ecs::world::Index;
-use amethyst::renderer::{Camera as AmethystCamera, Projection};
 
 use super::component_prelude::*;
 
 mod defaults {
     use super::Vector;
-    pub const SIZE: Vector = (100.0, 100.0);
     pub const BASE_SPEED: Vector = (250.0, 250.0);
     pub const DEADZONE: Vector = (16.0, 16.0);
 }
@@ -15,7 +13,6 @@ mod defaults {
 /// An entity with this component won't do much without it also having
 /// an `amethyst::renderer::Camera` component.
 pub struct Camera {
-    pub(crate) camera:     AmethystCamera,
     pub(crate) base_speed: Vector,
     pub(crate) deadzone:   Vector,
 
@@ -31,7 +28,6 @@ impl Camera {
 }
 
 pub struct CameraBuilder {
-    pub camera:     Option<AmethystCamera>,
     pub base_speed: Option<Vector>,
     pub deadzone:   Option<Vector>,
     pub follow:     Option<Index>,
@@ -39,12 +35,6 @@ pub struct CameraBuilder {
 
 /// A builder struct for `Camera`.
 impl CameraBuilder {
-    /// Set the amethyst camera.
-    pub fn camera(mut self, camera: AmethystCamera) -> Self {
-        self.camera = Some(camera);
-        self
-    }
-
     /// Set the `base_speed`.
     pub fn base_speed(mut self, base_speed: Vector) -> Self {
         self.base_speed = Some(base_speed);
@@ -66,7 +56,6 @@ impl CameraBuilder {
     /// Build the `Camera`.
     pub fn build(self) -> Camera {
         let default = Camera::default();
-        let camera = self.camera.unwrap_or(default.camera);
         let base_speed = self.base_speed.unwrap_or(default.base_speed);
         let deadzone = self.deadzone.unwrap_or(default.deadzone);
         let follow = if self.follow.is_some() {
@@ -75,7 +64,6 @@ impl CameraBuilder {
             default.follow
         };
         Camera {
-            camera,
             base_speed,
             deadzone,
             follow,
@@ -86,7 +74,6 @@ impl CameraBuilder {
 impl Default for CameraBuilder {
     fn default() -> Self {
         CameraBuilder {
-            camera:     None,
             base_speed: None,
             deadzone:   None,
             follow:     None,
@@ -102,12 +89,6 @@ impl Default for Camera {
     fn default() -> Self {
         use defaults::*;
         Camera {
-            camera:     AmethystCamera::from(Projection::orthographic(
-                0.0,    // left
-                SIZE.0, // right
-                0.0,    // bottom
-                SIZE.1, // top
-            )),
             base_speed: BASE_SPEED,
             deadzone:   DEADZONE,
             follow:     None,
