5c4afc20aa95293601bb80c0e8f9467096d245c0
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-04-13 15:45:19 +0200

    Added is_* methods to geo::Side

--- src/geo/side.rs
+++ src/geo/side.rs
@@ -8,3 +8,30 @@ pub enum Side {
     Left,
     Right,
 }
+
+impl Side {
+    /// Returns `true` when side is `Inner`.
+    pub fn is_inner(&self) -> bool {
+        self == &Side::Inner
+    }
+
+    /// Returns `true` when side is `Top`.
+    pub fn is_top(&self) -> bool {
+        self == &Side::Top
+    }
+
+    /// Returns `true` when side is `Bottom`.
+    pub fn is_bottom(&self) -> bool {
+        self == &Side::Bottom
+    }
+
+    /// Returns `true` when side is `Left`.
+    pub fn is_left(&self) -> bool {
+        self == &Side::Left
+    }
+
+    /// Returns `true` when side is `Right`.
+    pub fn is_right(&self) -> bool {
+        self == &Side::Right
+    }
+}
