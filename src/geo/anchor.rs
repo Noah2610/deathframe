cafa84f0260906bc302336e69ece1c4a1e6c5995
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-04-17 17:29:54 +0200

    Changed Anchor method headers

--- src/geo/anchor.rs
+++ src/geo/anchor.rs
@@ -18,9 +18,9 @@ pub enum Anchor {
 impl Anchor {
     /// Returns the top-left position as a `Vector`.
     #[rustfmt::skip]
-    pub fn top_left_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn top_left_for(&self, pos: &Vector, size: &Vector) -> Vector {
         match self {
-            Anchor::TopLeft      => pos,
+            Anchor::TopLeft      => *pos,
             Anchor::TopRight     => (pos.0 - size.0, pos.1).into(),
             Anchor::TopMiddle    => (pos.0 - size.0 * 0.5, pos.1).into(),
             Anchor::BottomLeft   => (pos.0, pos.1 + size.1).into(),
@@ -33,49 +33,49 @@ impl Anchor {
     }
 
     /// Returns the bottom-left position as a `Vector`.
-    pub fn bottom_left_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn bottom_left_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0, top_left.1 - size.1).into()
     }
 
     /// Returns the middle-left position as a `Vector`.
-    pub fn middle_left_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn middle_left_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0, top_left.1 - size.1 * 0.5).into()
     }
 
     /// Returns the top-right position as a `Vector`.
-    pub fn top_right_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn top_right_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0 + size.1, top_left.1).into()
     }
 
     /// Returns the bottom-right position as a `Vector`.
-    pub fn bottom_right_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn bottom_right_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0 + size.1, top_left.1 - size.1).into()
     }
 
     /// Returns the middle-right position as a `Vector`.
-    pub fn middle_right_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn middle_right_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0 + size.1, top_left.1 - size.1 * 0.5).into()
     }
 
     /// Returns the top-middle position as a `Vector`.
-    pub fn top_middle_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn top_middle_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0 + size.0 * 0.5, top_left.1).into()
     }
 
     /// Returns the bottom-middle position as a `Vector`.
-    pub fn bottom_middle_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn bottom_middle_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0 + size.0 * 0.5, top_left.1 - size.1).into()
     }
 
     /// Returns the middle position as a `Vector`.
-    pub fn middle_for(&self, pos: Vector, size: Vector) -> Vector {
+    pub fn middle_for(&self, pos: &Vector, size: &Vector) -> Vector {
         let top_left = self.top_left_for(pos, size);
         (top_left.0 + size.0 * 0.5, top_left.1 - size.1 * 0.5).into()
     }
