c8c384bc267c6cbd3be106a7183ed256641bdec2
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-04-29 19:01:15 +0200

    Added 'dont_limit_*' methods to components::MaxVelocity

--- src/components/max_velocity.rs
+++ src/components/max_velocity.rs
@@ -6,33 +6,57 @@ use super::component_prelude::*;
 /// Fields are both optional; if a field is `None`, then there is no cap for that value.
 #[derive(Serialize, Deserialize)]
 pub struct MaxVelocity {
-    pub x: Option<f32>,
-    pub y: Option<f32>,
+    pub x:                     Option<f32>,
+    pub y:                     Option<f32>,
+    pub(crate) should_limit_x: bool,
+    pub(crate) should_limit_y: bool,
 }
 
 impl MaxVelocity {
     /// Create a new `MaxVelocity` component with both `x` and `y` fields.
     pub fn with_xy(x: f32, y: f32) -> Self {
         Self {
-            x: Some(x),
-            y: Some(y),
+            x:              Some(x),
+            y:              Some(y),
+            should_limit_x: true,
+            should_limit_y: true,
         }
     }
 
     /// Create a new `MaxVelocity` component with only a max `x` value.
     pub fn with_x(x: f32) -> Self {
         Self {
-            x: Some(x),
-            y: None,
+            x:              Some(x),
+            y:              None,
+            should_limit_x: true,
+            should_limit_y: true,
         }
     }
 
     /// Create a new `MaxVelocity` component with only a max `y` value.
     pub fn with_y(y: f32) -> Self {
         Self {
-            x: None,
-            y: Some(y),
+            x:              None,
+            y:              Some(y),
+            should_limit_x: true,
+            should_limit_y: true,
         }
+    }
+
+    /// Skip velocity limitation next frame.
+    pub fn dont_limit(&mut self) {
+        self.dont_limit_x();
+        self.dont_limit_y();
+    }
+
+    /// Skip velocity limitation (x) next frame.
+    pub fn dont_limit_x(&mut self) {
+        self.should_limit_x = true;
+    }
+
+    /// Skip velocity limitation (y) next frame.
+    pub fn dont_limit_y(&mut self) {
+        self.should_limit_y = true;
     }
 }
 
@@ -55,8 +79,10 @@ impl From<Vector<f32>> for MaxVelocity {
 impl From<(Option<f32>, Option<f32>)> for MaxVelocity {
     fn from(data: (Option<f32>, Option<f32>)) -> Self {
         Self {
-            x: data.0,
-            y: data.1,
+            x:              data.0,
+            y:              data.1,
+            should_limit_x: true,
+            should_limit_y: true,
         }
     }
 }
--- src/systems/limit_velocities.rs
+++ src/systems/limit_velocities.rs
@@ -6,23 +6,31 @@ pub struct LimitVelocitiesSystem;
 
 impl<'a> System<'a> for LimitVelocitiesSystem {
     type SystemData =
-        (ReadStorage<'a, MaxVelocity>, WriteStorage<'a, Velocity>);
+        (WriteStorage<'a, MaxVelocity>, WriteStorage<'a, Velocity>);
 
-    fn run(&mut self, (max_velocities, mut velocities): Self::SystemData) {
-        for (max, velocity) in (&max_velocities, &mut velocities).join() {
-            if let Some(max) = max.x {
-                if velocity.x > 0.0 {
-                    velocity.x = velocity.x.min(max)
-                } else if velocity.x < 0.0 {
-                    velocity.x = velocity.x.max(-max)
+    fn run(&mut self, (mut max_velocities, mut velocities): Self::SystemData) {
+        for (max, velocity) in (&mut max_velocities, &mut velocities).join() {
+            if max.should_limit_x {
+                if let Some(max) = max.x {
+                    if velocity.x > 0.0 {
+                        velocity.x = velocity.x.min(max)
+                    } else if velocity.x < 0.0 {
+                        velocity.x = velocity.x.max(-max)
+                    }
                 }
+            } else {
+                max.should_limit_x = true;
             }
-            if let Some(max) = max.y {
-                if velocity.y > 0.0 {
-                    velocity.y = velocity.y.min(max)
-                } else if velocity.y < 0.0 {
-                    velocity.y = velocity.y.max(-max)
+            if max.should_limit_y {
+                if let Some(max) = max.y {
+                    if velocity.y > 0.0 {
+                        velocity.y = velocity.y.min(max)
+                    } else if velocity.y < 0.0 {
+                        velocity.y = velocity.y.max(-max)
+                    }
                 }
+            } else {
+                max.should_limit_y = true;
             }
         }
     }
