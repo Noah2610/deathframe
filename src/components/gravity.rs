b3e8c098fa40e4030900803e2f0dc119a60d54a7
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-07-07 21:24:46 +0200

    Added enable and disable methods to Gravity component; use $EDITOR variable in bin/vimall

--- src/components/gravity.rs
+++ src/components/gravity.rs
@@ -4,14 +4,29 @@ use super::component_prelude::*;
 /// This means, their velocity is increased by the gravity's force (`x`, `y`) every frame.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Gravity {
-    pub x: f32,
-    pub y: f32,
+    pub x:              f32,
+    pub y:              f32,
+    pub(crate) enabled: bool,
 }
 
 impl Gravity {
     /// Create a new `Gravity` component with the given `x` and `y` values.
     pub fn new(x: f32, y: f32) -> Self {
-        Self { x, y }
+        Self {
+            x,
+            y,
+            enabled: true,
+        }
+    }
+
+    /// Enable the gravity, if it was previously disabled via the `disable` method.
+    pub fn enable(&mut self) {
+        self.enabled = true;
+    }
+
+    /// Disable the gravity, until re-enabled via the `enable` method.
+    pub fn disable(&mut self) {
+        self.enabled = false;
     }
 }
 
--- src/systems/gravity.rs
+++ src/systems/gravity.rs
@@ -20,25 +20,27 @@ impl<'a> System<'a> for GravitySystem {
         for (gravity, velocity, mut decr_velocity) in
             (&gravities, &mut velocities, (&mut decr_velocities).maybe()).join()
         {
-            if gravity.x != 0.0 {
-                velocity.x += gravity.x * dt;
-                decr_velocity.as_mut().map(|decr| {
-                    if gravity.x > 0.0 {
-                        decr.should_decrease_x_pos = false;
-                    } else if gravity.x < 0.0 {
-                        decr.should_decrease_x_neg = false;
-                    }
-                });
-            }
-            if gravity.y != 0.0 {
-                velocity.y += gravity.y * dt;
-                decr_velocity.as_mut().map(|decr| {
-                    if gravity.y > 0.0 {
-                        decr.should_decrease_y_pos = false;
-                    } else if gravity.y < 0.0 {
-                        decr.should_decrease_y_neg = false;
-                    }
-                });
+            if gravity.enabled {
+                if gravity.x != 0.0 {
+                    velocity.x += gravity.x * dt;
+                    decr_velocity.as_mut().map(|decr| {
+                        if gravity.x > 0.0 {
+                            decr.should_decrease_x_pos = false;
+                        } else if gravity.x < 0.0 {
+                            decr.should_decrease_x_neg = false;
+                        }
+                    });
+                }
+                if gravity.y != 0.0 {
+                    velocity.y += gravity.y * dt;
+                    decr_velocity.as_mut().map(|decr| {
+                        if gravity.y > 0.0 {
+                            decr.should_decrease_y_pos = false;
+                        } else if gravity.y < 0.0 {
+                            decr.should_decrease_y_neg = false;
+                        }
+                    });
+                }
             }
         }
     }
