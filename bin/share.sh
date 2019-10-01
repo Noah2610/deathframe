d81ad0b4be4c4a4b5b1aae3c9d06c0eb2c053ac1
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-10-02 00:52:40 +0200

    Improve bin/ scripts

--- bin/share.sh
+++ bin/share.sh
@@ -7,5 +7,17 @@ unset _dir
 # https://stackoverflow.com/a/17841619/10927893
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
+function cargo_cmd {
+  check "cargo"
+  local cargo_cmd="$1"
+  shift
+  cmd="cargo +$RUST_VERSION $cargo_cmd --features amethyst/nightly $*"
+  if should_run_in_terminal; then
+    run_terminal "$cmd"
+  else
+    $cmd
+  fi
+}
+
 LOGFILE="${ROOT}/logs/$( basename "$0" ).log"
 RUST_VERSION="nightly-2019-08-13"
