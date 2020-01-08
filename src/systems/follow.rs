f059f038eefa93654fe7df8fd97edc74d63b1457
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-01-08 15:53:51 +0100

    Write FollowSystem

--- src/systems/follow.rs
+++ src/systems/follow.rs
@@ -4,8 +4,33 @@ use super::system_prelude::*;
 pub struct FollowSystem;
 
 impl<'a> System<'a> for FollowSystem {
-    type SystemData = ();
+    type SystemData = (
+        Entities<'a>,
+        ReadStorage<'a, Follow>,
+        WriteStorage<'a, Transform>,
+    );
 
-    fn run(&mut self, (): Self::SystemData) {
+    fn run(&mut self, (entities, followers, mut transforms): Self::SystemData) {
+        let mut followers_join =
+            (&entities, &followers).join().collect::<Vec<_>>();
+        followers_join.sort_by(|(_, follower_a), (_, follower_b)| {
+            follower_a.partial_cmp(follower_b).unwrap()
+        });
+
+        for (follower_entity, follower) in followers_join {
+            if let Some(followed_pos) =
+                transforms.get(follower.to_follow).map(|transform| {
+                    let translation = transform.translation();
+                    (translation.x, translation.y)
+                })
+            {
+                if let Some(follower_transform) =
+                    transforms.get_mut(follower_entity)
+                {
+                    follower_transform.set_translation_x(followed_pos.0);
+                    follower_transform.set_translation_y(followed_pos.1);
+                }
+            }
+        }
     }
 }
