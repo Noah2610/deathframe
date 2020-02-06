990e2f8f0e2ac99f25b1e2edc096c48bfc275f60
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-06 15:35:59 +0100

    Re-export some modules from core/physics crates in root deathframe crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,36 @@ pub extern crate core;
 #[cfg(feature = "physics")]
 pub extern crate physics;
 
+pub mod components {
+    pub mod prelude {
+        pub use core::components::prelude::*;
+        #[cfg(feature = "physics")]
+        pub use physics::components::prelude::*;
+    }
+
+    pub use core::components::*;
+    #[cfg(feature = "physics")]
+    pub use physics::components::*;
+}
+
+pub mod systems {
+    pub mod prelude {
+        pub use core::systems::prelude::*;
+        #[cfg(feature = "physics")]
+        pub use physics::systems::prelude::*;
+    }
+
+    pub use core::systems::*;
+    #[cfg(feature = "physics")]
+    pub use physics::systems::*;
+}
+
+pub mod resources {
+    pub mod prelude {
+        pub use core::resources::prelude::*;
+    }
+
+    pub use core::resources::*;
+}
+
 pub use core::amethyst;
