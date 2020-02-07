af4387331b2e4c577396734aac3adbed74b96378
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-07 17:42:27 +0100

    Remove core::geo::side

--- deathframe_core/src/geo/mod.rs
+++ deathframe_core/src/geo/mod.rs
@@ -2,11 +2,9 @@
 //! (or algebra, not sure what to call it exactly).
 
 mod axis;
-mod side;
 
 pub mod prelude {
     pub use super::Axis;
-    pub use super::Side;
     pub use super::Vector;
 }
 
@@ -16,4 +14,3 @@ pub type Vector<T = f32> = math::Vector2<T>;
 pub type Point<T = f32> = math::Point2<T>;
 
 pub use axis::Axis;
-pub use side::Side;
