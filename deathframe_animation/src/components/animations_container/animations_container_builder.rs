d4cfcf8b3d8f2113b67020f2e4a155111677b79a
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-15 01:06:13 +0100

    Add current_animation and methods to AnimationsContainerBuilder

--- deathframe_animation/src/components/animations_container/animations_container_builder.rs
+++ deathframe_animation/src/components/animations_container/animations_container_builder.rs
@@ -6,7 +6,40 @@ pub struct AnimationsContainerBuilder<K>
 where
     K: 'static + Hash + Eq + Send + Sync,
 {
-    animations: HashMap<K, Animation>,
+    animations:        HashMap<K, Animation>,
+    current_animation: Option<K>,
+}
+
+impl<K> AnimationsContainerBuilder<K>
+where
+    K: 'static + Hash + Eq + Send + Sync,
+{
+    /// Add an `Animation` associated to a _key_ to the `AnimationsContainer`.
+    pub fn with(mut self, key: K, animation: Animation) -> Self {
+        self.animations.insert(key, animation);
+        self
+    }
+
+    /// Set the initally current animation.
+    /// An animation with the given _key_ must have been added previously.
+    pub fn current(mut self, key: K) -> Result<Self, String> {
+        if !self.animations.contains_key(&key) {
+            return Err(String::from(
+                "Given key to AnimationsContainerBuilder::current doesn't \
+                 exist as animation",
+            ));
+        }
+        self.current_animation = Some(key);
+        Ok(self)
+    }
+
+    /// Build the `AnimationsContainer`.
+    pub fn build(self) -> Result<AnimationsContainer<K>, ()> {
+        Ok(AnimationsContainer {
+            animations:        self.animations,
+            current_animation: self.current_animation,
+        })
+    }
 }
 
 impl<K> Default for AnimationsContainerBuilder<K>
@@ -15,19 +48,8 @@ where
 {
     fn default() -> Self {
         Self {
-            animations: HashMap::new(),
-        }
-    }
-}
-
-impl<K> AnimationsContainerBuilder<K>
-where
-    K: 'static + Hash + Eq + Send + Sync,
-{
-    pub fn build(self) -> Result<AnimationsContainer<K>, ()> {
-        Ok(AnimationsContainer {
-            animations:        self.animations,
+            animations:        HashMap::new(),
             current_animation: None,
-        })
+        }
     }
 }
