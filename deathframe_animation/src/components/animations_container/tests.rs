ae4c231734c1bca1004e50c2fce71f042dfd5ea6
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-15 16:01:22 +0100

    Add AnimationsContainer tests

--- deathframe_animation/src/components/animations_container/tests.rs
+++ deathframe_animation/src/components/animations_container/tests.rs
@@ -3,6 +3,39 @@
 use crate::components::prelude::*;
 
 #[test]
-fn can_build_animations_container() {
+fn can_build_empty_animations_container() {
     let _ = AnimationsContainer::<()>::builder().build().unwrap();
+}
+
+#[test]
+fn can_build_animations_container() {
+    let _ = AnimationsContainer::<String>::builder()
+        .with("FIRST_ANIM".into(), || {
+            Box::new(
+                vec![
+                    AnimationFrame::builder()
+                        .sprite_id(0)
+                        .duration_ms(1000)
+                        .build()
+                        .unwrap(),
+                    (1_u8, 100_u8).into(),
+                    (2_usize, 1000_u64).into(),
+                    (3_usize, 9999999_u64).into(),
+                ]
+                .into_iter(),
+            )
+        })
+        .with("SECOND_ANIM".into(), || {
+            Box::new(
+                vec![
+                    (1_usize, 500_u64).into(),
+                    (1_usize, 500_u64).into(),
+                    (1_usize, 500_u64).into(),
+                ]
+                .into_iter()
+                .cycle(),
+            )
+        })
+        .build()
+        .unwrap();
 }
