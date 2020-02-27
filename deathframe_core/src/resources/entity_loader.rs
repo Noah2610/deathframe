a6ba64d32fbc3dcebbb2bd6837d72a1d10301755
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-27 19:08:18 +0100

    Add EntityLoader resource to core

---
+++ deathframe_core/src/resources/entity_loader.rs
@@ -0,0 +1,60 @@
+use crate::components::prelude::Loaded;
+use amethyst::ecs::{Entity, WriteStorage};
+use std::collections::HashMap;
+
+/// The load action to perform for an entity.
+#[derive(PartialEq)]
+enum LoadAction {
+    Load,
+    Unload,
+}
+
+/// The `EntityLoader` is filled with entities that need to be
+/// loaded, unloaded, and executes all loading
+/// actions at once with the `run` method.
+/// _Loading_ takes precedence over _unloading_, so if an entity
+/// is staged for loading _and_ unloading, then it will be loaded.
+#[derive(Default)]
+pub struct EntityLoader {
+    load_actions: HashMap<Entity, LoadAction>,
+}
+
+impl EntityLoader {
+    /// The given entity should be loaded, if it was unloaded.
+    pub fn load(&mut self, entity: Entity) {
+        self.load_actions.insert(entity, LoadAction::Load);
+    }
+
+    /// The given entity should be unloaded, if it was loaded.
+    pub fn unload(&mut self, entity: Entity) {
+        // Only unload if it isn't already staged for loading.
+        let is_staged_for_loading = self
+            .load_actions
+            .get(&entity)
+            .map(|action| action == &LoadAction::Load)
+            .unwrap_or(false);
+
+        if !is_staged_for_loading {
+            self.load_actions.insert(entity, LoadAction::Unload);
+        }
+    }
+
+    pub fn run(self, loadeds: &mut WriteStorage<Loaded>) {
+        for (entity, load_action) in self.load_actions {
+            match load_action {
+                LoadAction::Load => {
+                    // Load, unless it is already loaded
+                    if !loadeds.contains(entity) {
+                        loadeds.insert(entity, Loaded);
+                    }
+                }
+                LoadAction::Unload => {
+                    // Unload, unless it is already unloaded
+                    if loadeds.contains(entity) {
+                        loadeds.remove(entity);
+                    }
+                }
+            }
+        }
+    }
+}
