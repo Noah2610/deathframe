02d49270927169dad3d544c16e7148c1d338e503
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-03-10 17:12:09 +0100

    Rework collider Query, run method return Vec of matched collisions
    Instead of returning a boolean, the Query::run method returns
    a Vec of collision references for all collisions matched,
    between both query types (find, fiter).
    
    Query types _any_, _all_ renamed to _find_, _filter_,
    which makes more sense with the new functionality.

--- deathframe_physics/src/query/mod.rs
+++ deathframe_physics/src/query/mod.rs
@@ -11,9 +11,9 @@ pub struct Query<'a, C>
 where
     C: 'static + CollisionTag,
 {
-    collider:       &'a Collider<C>,
-    expression_any: Option<QExp<C>>,
-    expression_all: Option<QExp<C>>,
+    collider:           &'a Collider<C>,
+    find_expressions:   Vec<QExp<C>>,
+    filter_expressions: Vec<QExp<C>>,
 }
 
 impl<'a, C> Query<'a, C>
@@ -24,20 +24,20 @@ where
     pub fn new(collider: &'a Collider<C>) -> Self {
         Self {
             collider,
-            expression_any: None,
-            expression_all: None,
+            find_expressions: Default::default(),
+            filter_expressions: Default::default(),
         }
     }
 
-    /// Checks if the given `QExp` matches _any_ collision.
-    pub fn any(mut self, exp: QExp<C>) -> Self {
-        self.expression_any = Some(exp);
+    /// Checks and adds the _first_ collision that matches the given `QExp`.
+    pub fn find(mut self, exp: QExp<C>) -> Self {
+        self.find_expressions.push(exp);
         self
     }
 
-    /// Checks if the given `QExp` matches _all_ collisions.
-    pub fn all(mut self, exp: QExp<C>) -> Self {
-        self.expression_all = Some(exp);
+    /// Checks and adds _all_ collisions that match the given `QExp`.
+    pub fn filter(mut self, exp: QExp<C>) -> Self {
+        self.filter_expressions.push(exp);
         self
     }
 
@@ -73,32 +73,35 @@ where
     }
 
     /// Run the query.
-    /// Runs both _any_ and _all_ queries (if both exist) and checks
-    /// if all existing queries return `true`.
-    pub fn run(mut self) -> bool {
-        let query_any =
-            |query: &mut Self| {
-                query.expression_any.take().map(|exp| {
-                    query.collider.collisions.values().any(|collision| {
-                        query.run_expression_on(&exp, collision)
+    /// Runs both _any_ and _all_ queries (if both exist)
+    /// and returns all collisions that match the queries.
+    pub fn run(mut self) -> Vec<&'a CollisionData<C>> {
+        let query_find = |query: &Self| -> Vec<&'a CollisionData<C>> {
+            query
+                .find_expressions
+                .iter()
+                .filter_map(|exp| {
+                    query.collider.collisions.values().find(move |collision| {
+                        query.run_expression_on(exp, collision)
                     })
                 })
-            };
-        let query_all =
-            |query: &mut Self| {
-                query.expression_all.take().map(|exp| {
-                    query.collider.collisions.values().all(|collision| {
-                        query.run_expression_on(&exp, collision)
-                    })
+                .collect()
+        };
+        let query_filter = |query: &Self| -> Vec<&'a CollisionData<C>> {
+            query
+                .filter_expressions
+                .iter()
+                .map(|exp| {
+                    query.collider.collisions.values().filter(
+                        move |collision| {
+                            query.run_expression_on(exp, collision)
+                        },
+                    )
                 })
-            };
+                .flatten()
+                .collect()
+        };
 
-        match (query_any(&mut self), query_all(&mut self)) {
-            (Some(result_any), Some(result_all)) => {
-                result_any == true && result_all == true
-            }
-            (Some(result), None) | (None, Some(result)) => result,
-            (None, None) => false,
-        }
+        [query_find(&mut self), query_filter(&mut self)].concat()
     }
 }
