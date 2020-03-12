c4eb9d48f06529307b6fdfda2653b208f099fdf1
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-03-12 21:40:28 +0100

    Add filter_ids to FindQuery
    Issue #6

--- deathframe_physics/src/query/find_query.rs
+++ deathframe_physics/src/query/find_query.rs
@@ -3,6 +3,7 @@ pub mod prelude {
 }
 
 use super::query_prelude::*;
+use specs::world::Index;
 
 /// The `FindQuery` runs a given `QueryExpression` on all
 /// collisions, and returns the first match.
@@ -13,6 +14,7 @@ where
 {
     collider:   &'a Collider<C>,
     expression: Option<QueryExpression<C>>,
+    filter_ids: Option<Vec<Index>>,
 }
 
 impl<'a, C> FindQuery<'a, C>
@@ -22,6 +24,12 @@ where
     /// Use the given `QueryExpression` to match collisions when running the query.
     pub fn exp(mut self, exp: QueryExpression<C>) -> Self {
         self.expression = Some(exp);
+        self
+    }
+
+    /// If given, only match collisions for entities that have one of the given IDs.
+    pub fn filter_ids(mut self, ids: Vec<Index>) -> Self {
+        self.filter_ids = Some(ids);
         self
     }
 }
@@ -36,16 +44,25 @@ where
         let Self {
             collider,
             expression,
+            filter_ids,
         } = self;
 
         let exp = expression?;
 
-        let matched_collisions = collider
-            .collisions
-            .values()
-            .find(|collision| does_expression_match_collision(&exp, collision));
+        let find_predicate = |collision: &&CollisionData<C>| -> bool {
+            does_expression_match_collision(&exp, *collision)
+        };
 
-        matched_collisions
+        let found_collision = if let Some(filter_ids) = filter_ids {
+            filter_ids
+                .iter()
+                .filter_map(|id| collider.collisions.get(id))
+                .find(find_predicate)
+        } else {
+            collider.collisions.values().find(find_predicate)
+        };
+
+        found_collision
     }
 }
 
@@ -57,6 +74,7 @@ where
         Self {
             collider,
             expression: None,
+            filter_ids: None,
         }
     }
 }
