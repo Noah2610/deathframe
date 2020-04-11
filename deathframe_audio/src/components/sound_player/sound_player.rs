2df65692b0a2feb782497265be55240541071580
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-04-11 02:44:58 +0200

    Manually impl Default for SoundPlayer

--- deathframe_audio/src/components/sound_player/sound_player.rs
+++ deathframe_audio/src/components/sound_player/sound_player.rs
@@ -2,7 +2,7 @@ use super::component_prelude::*;
 use super::sound_action::SoundAction;
 use std::hash::Hash;
 
-#[derive(Component, Default)]
+#[derive(Component)]
 #[storage(VecStorage)]
 pub struct SoundPlayer<K>
 where
@@ -18,5 +18,16 @@ where
     type Action = SoundAction<K>;
     fn mut_actions(&mut self) -> &mut Vec<Self::Action> {
         &mut self.actions
+    }
+}
+
+impl<K> Default for SoundPlayer<K>
+where
+    K: PartialEq + Eq + Hash + Send + Sync,
+{
+    fn default() -> Self {
+        Self {
+            actions: Vec::new(),
+        }
     }
 }
