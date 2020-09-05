0535e35c301b1a5788c50f7a6f77bb44bd230637
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-09-05 19:45:28 +0200

    Add method Health::has_full_health

--- deathframe_core/src/components/health/mod.rs
+++ deathframe_core/src/components/health/mod.rs
@@ -21,15 +21,23 @@ pub struct Health {
 }
 
 impl Health {
+    /// Gain the given amount of hitpoints, without exceeding its max health.
     pub fn gain(&mut self, hp: HitPoints) {
         self.health = (self.health + hp).min(self.max_health);
     }
 
+    /// Lose the given amount of hitpoints, wihout going below 0.
     pub fn lose(&mut self, hp: HitPoints) {
         self.health = self.health.checked_sub(hp).unwrap_or(0);
     }
 
+    /// Checks if health is above 0.
     pub fn is_alive(&self) -> bool {
         self.health > 0
+    }
+
+    /// Checks if the health is at max health.
+    pub fn has_full_health(&self) -> bool {
+        self.health == self.max_health
     }
 }
